fc0d66d11861f0bc3525d2e0d87bcbd38e075fa1
Tomas Baca <klaxalk@gmail.com>
2023-07-31 11:37:19 +0200

    fixing dep

--- .ci/build_package.sh
+++ .ci/build_package.sh
@@ -35,7 +35,8 @@ rosdep install -y -v --rosdistro=noetic --from-paths ./
 sudo apt-get -y install ros-noetic-catkin python3-catkin-tools
 
 # PX4-specific dependency
-sudo pip3 install -U kconfiglib
+# sudo pip3 install -U kconfiglib
+python3 -m pip install -r $GITHUB_WORKSPACE/Tools/setup/requirements.txt
 # $GITHUB_WORKSPACE/Tools/setup/ubuntu.sh --no-nuttx --no-sim-tool
 
 ## | ---------------- prepare catkin workspace ---------------- |
